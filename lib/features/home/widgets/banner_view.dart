f43cc7b45cfc0e7b1e703847f685e1c227ed6121
Mohammed Riyad <riyadm2001@gmail.com>
2024-11-05 19:45:51 +0200

    change main banner design

--- lib/features/home/widgets/banner_view.dart
+++ lib/features/home/widgets/banner_view.dart
@@ -10,7 +10,6 @@ import 'package:sixam_mart/features/location/domain/models/zone_response_model.d
 import 'package:sixam_mart/helper/address_helper.dart';
 import 'package:sixam_mart/helper/route_helper.dart';
 import 'package:sixam_mart/util/dimensions.dart';
-import 'package:sixam_mart/util/styles.dart';
 import 'package:sixam_mart/common/widgets/custom_image.dart';
 import 'package:sixam_mart/common/widgets/custom_snackbar.dart';
 import 'package:sixam_mart/features/store/screens/store_screen.dart';
@@ -25,121 +24,185 @@ class BannerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return GetBuilder<BannerController>(builder: (bannerController) {
-      List<String?>? bannerList = isFeatured ? bannerController.featuredBannerList : bannerController.bannerImageList;
-      List<dynamic>? bannerDataList = isFeatured ? bannerController.featuredBannerDataList : bannerController.bannerDataList;
+      List<String?>? bannerList = isFeatured
+          ? bannerController.featuredBannerList
+          : bannerController.bannerImageList;
+      List<dynamic>? bannerDataList = isFeatured
+          ? bannerController.featuredBannerDataList
+          : bannerController.bannerDataList;
 
-      return (bannerList != null && bannerList.isEmpty) ? const SizedBox() : Container(
-        width: MediaQuery.of(context).size.width,
-        height: GetPlatform.isDesktop ? 500 : MediaQuery.of(context).size.width * 0.45,
-        padding: const EdgeInsets.only(top: Dimensions.paddingSizeDefault),
-        child: bannerList != null ? Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: CarouselSlider.builder(
-                options: CarouselOptions(
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  disableCenter: true,
-                  viewportFraction: 0.8,
-                  autoPlayInterval: const Duration(seconds: 7),
-                  onPageChanged: (index, reason) {
-                    bannerController.setCurrentIndex(index, true);
-                  },
-                ),
-                itemCount: bannerList.isEmpty ? 1 : bannerList.length,
-                itemBuilder: (context, index, _) {
+      return (bannerList != null && bannerList.isEmpty)
+          ? const SizedBox()
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              height: GetPlatform.isDesktop
+                  ? 500
+                  : MediaQuery.of(context).size.width * 0.55,
+              padding:
+                  const EdgeInsets.only(top: Dimensions.paddingSizeDefault),
+              child: bannerList != null
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: CarouselSlider.builder(
+                            options: CarouselOptions(
+                              autoPlay: true,
+                              enlargeCenterPage: true,
+                              disableCenter: true,
+                              viewportFraction: 0.85,
+                              autoPlayInterval: const Duration(seconds: 10),
+                              onPageChanged: (index, reason) {
+                                bannerController.setCurrentIndex(index, true);
+                              },
+                            ),
+                            itemCount:
+                                bannerList.isEmpty ? 1 : bannerList.length,
+                            itemBuilder: (context, index, _) {
+                              return InkWell(
+                                onTap: () async {
+                                  if (bannerDataList![index] is Item) {
+                                    Item? item = bannerDataList[index];
+                                    Get.find<ItemController>()
+                                        .navigateToItemPage(item, context);
+                                  } else if (bannerDataList[index] is Store) {
+                                    Store? store = bannerDataList[index];
+                                    if (isFeatured &&
+                                        (AddressHelper.getUserAddressFromSharedPref()!
+                                                    .zoneData !=
+                                                null &&
+                                            AddressHelper
+                                                    .getUserAddressFromSharedPref()!
+                                                .zoneData!
+                                                .isNotEmpty)) {
+                                      for (ModuleModel module
+                                          in Get.find<SplashController>()
+                                              .moduleList!) {
+                                        if (module.id == store!.moduleId) {
+                                          Get.find<SplashController>()
+                                              .setModule(module);
+                                          break;
+                                        }
+                                      }
+                                      ZoneData zoneData = AddressHelper
+                                              .getUserAddressFromSharedPref()!
+                                          .zoneData!
+                                          .firstWhere((data) =>
+                                              data.id == store!.zoneId);
 
-                  return InkWell(
-                    onTap: () async {
-                      if(bannerDataList![index] is Item) {
-                        Item? item = bannerDataList[index];
-                        Get.find<ItemController>().navigateToItemPage(item, context);
-                      }else if(bannerDataList[index] is Store) {
-                        Store? store = bannerDataList[index];
-                        if(isFeatured && (AddressHelper.getUserAddressFromSharedPref()!.zoneData != null && AddressHelper.getUserAddressFromSharedPref()!.zoneData!.isNotEmpty)) {
-                          for(ModuleModel module in Get.find<SplashController>().moduleList!) {
-                            if(module.id == store!.moduleId) {
-                              Get.find<SplashController>().setModule(module);
-                              break;
-                            }
-                          }
-                          ZoneData zoneData = AddressHelper.getUserAddressFromSharedPref()!.zoneData!.firstWhere((data) => data.id == store!.zoneId);
-
-                          Modules module = zoneData.modules!.firstWhere((module) => module.id == store!.moduleId);
-                          Get.find<SplashController>().setModule(ModuleModel(id: module.id, moduleName: module.moduleName, moduleType: module.moduleType, themeId: module.themeId, storesCount: module.storesCount));
-                        }
-                        Get.toNamed(
-                          RouteHelper.getStoreRoute(id: store!.id, page: isFeatured ? 'module' : 'banner'),
-                          arguments: StoreScreen(store: store, fromModule: isFeatured),
-                        );
-                      }else if(bannerDataList[index] is BasicCampaignModel) {
-                        BasicCampaignModel campaign = bannerDataList[index];
-                        Get.toNamed(RouteHelper.getBasicCampaignRoute(campaign));
-                      }else {
-                        String url = bannerDataList[index];
-                        if (await canLaunchUrlString(url)) {
-                          await launchUrlString(url, mode: LaunchMode.externalApplication);
-                        }else {
-                          showCustomSnackBar('unable_to_found_url'.tr);
-                        }
-                      }
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
-                        boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 5, spreadRadius: 0)],
-                      ),
-                      margin: const EdgeInsets.symmetric(vertical: Dimensions.paddingSizeExtraSmall),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
-                        child: GetBuilder<SplashController>(builder: (splashController) {
-                          return CustomImage(
-                            image: '${bannerList[index]}',
-                            fit: BoxFit.cover,
-                          );
-                        }),
-                      ),
+                                      Modules module = zoneData.modules!
+                                          .firstWhere((module) =>
+                                              module.id == store!.moduleId);
+                                      Get.find<SplashController>().setModule(
+                                          ModuleModel(
+                                              id: module.id,
+                                              moduleName: module.moduleName,
+                                              moduleType: module.moduleType,
+                                              themeId: module.themeId,
+                                              storesCount: module.storesCount));
+                                    }
+                                    Get.toNamed(
+                                      RouteHelper.getStoreRoute(
+                                          id: store!.id,
+                                          page:
+                                              isFeatured ? 'module' : 'banner'),
+                                      arguments: StoreScreen(
+                                          store: store, fromModule: isFeatured),
+                                    );
+                                  } else if (bannerDataList[index]
+                                      is BasicCampaignModel) {
+                                    BasicCampaignModel campaign =
+                                        bannerDataList[index];
+                                    Get.toNamed(
+                                        RouteHelper.getBasicCampaignRoute(
+                                            campaign));
+                                  } else {
+                                    String url = bannerDataList[index];
+                                    if (await canLaunchUrlString(url)) {
+                                      await launchUrlString(url,
+                                          mode: LaunchMode.externalApplication);
+                                    } else {
+                                      showCustomSnackBar(
+                                          'unable_to_found_url'.tr);
+                                    }
+                                  }
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).cardColor,
+                                    borderRadius: BorderRadius.circular(
+                                        Dimensions.radiusDefault),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                          color: Colors.black12,
+                                          blurRadius: 5,
+                                          spreadRadius: 0)
+                                    ],
+                                  ),
+                                  margin: const EdgeInsets.symmetric(
+                                      vertical:
+                                          Dimensions.paddingSizeExtraSmall),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(
+                                        Dimensions.radiusDefault),
+                                    child: GetBuilder<SplashController>(
+                                        builder: (splashController) {
+                                      return CustomImage(
+                                        image: '${bannerList[index]}',
+                                        fit: BoxFit.cover,
+                                      );
+                                    }),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                            height: Dimensions.paddingSizeExtraSmall),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: bannerList.map((bnr) {
+                            int index = bannerList.indexOf(bnr);
+                            return Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 3),
+                                child: AnimatedContainer(
+                                  duration: const Duration(milliseconds: 200),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        index == bannerController.currentIndex
+                                            ? Theme.of(context).primaryColor
+                                            : Theme.of(context)
+                                                .primaryColor
+                                                .withOpacity(0.5),
+                                    borderRadius: BorderRadius.circular(
+                                        Dimensions.radiusDefault),
+                                  ),
+                                  height: index == bannerController.currentIndex
+                                      ? 10
+                                      : 5,
+                                  width: index == bannerController.currentIndex
+                                      ? 10
+                                      : 5,
+                                ));
+                          }).toList(),
+                        ),
+                      ],
+                    )
+                  : Shimmer(
+                      duration: const Duration(seconds: 2),
+                      enabled: bannerList == null,
+                      child: Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.circular(Dimensions.radiusSmall),
+                            color: Colors.grey[300],
+                          )),
                     ),
-                  );
-                },
-              ),
-            ),
-
-            const SizedBox(height: Dimensions.paddingSizeExtraSmall),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: bannerList.map((bnr) {
-                int index = bannerList.indexOf(bnr);
-                int totalBanner = bannerList.length;
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 3),
-                  child: index == bannerController.currentIndex ? Container(
-                    decoration: BoxDecoration(color: Theme.of(context).primaryColor, borderRadius: BorderRadius.circular(Dimensions.radiusDefault)),
-                    padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 1),
-                    child: Text('${(index) + 1}/$totalBanner', style: robotoRegular.copyWith(color: Theme.of(context).cardColor, fontSize: 12)),
-                  ) : Container(
-                    height: 5, width: 6,
-                    decoration: BoxDecoration(color: Theme.of(context).primaryColor.withOpacity(0.5), borderRadius: BorderRadius.circular(Dimensions.radiusDefault)),
-                  ),
-                );
-              }).toList(),
-            ),
-
-          ],
-        ) : Shimmer(
-          duration: const Duration(seconds: 2),
-          enabled: bannerList == null,
-          child: Container(margin: const EdgeInsets.symmetric(horizontal: 10), decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(Dimensions.radiusSmall),
-            color: Colors.grey[300],
-          )),
-        ),
-      );
+            );
     });
   }
-
 }
