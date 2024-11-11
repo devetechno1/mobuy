5f7c9e4bc5dd20aa0fad2141fb387138c9c7d458
Mohammed Riyad <riyadm2001@gmail.com>
2024-11-11 14:44:46 +0200

    change app theme

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color(0xFFF0E517)}) => ThemeData(
   fontFamily: AppConstants.fontFamily,
   primaryColor: color,
-  secondaryHeaderColor: const Color(0xFF009f67),
+  secondaryHeaderColor: const Color(0xFFFDF67A),
   disabledColor: const Color(0xffa2a7ad),
   brightness: Brightness.dark,
   hintColor: const Color(0xFFbebebe),
@@ -12,7 +12,7 @@ ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
   shadowColor: Colors.white.withOpacity(0.03),
   textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white70)),
   textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: color)),
-  colorScheme: ColorScheme.dark(primary: color, secondary: color).copyWith(surface: const Color(0xFF191A26)).copyWith(error: const Color(0xFFdd3135)),
+  colorScheme: ColorScheme.dark(primary: color, secondary: const Color(0xFF1A1A1A)).copyWith(surface: const Color(0xFF191A26)).copyWith(error: const Color(0xFFdd3135)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white10),
   floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData light({Color color = const Color(0xFF039D55)}) => ThemeData(
+ThemeData light({Color color = const Color(0xFFF0E517)}) => ThemeData(
   fontFamily: AppConstants.fontFamily,
   primaryColor: color,
-  secondaryHeaderColor: const Color(0xFF1ED7AA),
+  secondaryHeaderColor: const Color(0xFFFDF67A),
   disabledColor: const Color(0xFFBABFC4),
   brightness: Brightness.light,
   hintColor: const Color(0xFF9F9F9F),
   cardColor: Colors.white,
   shadowColor: Colors.black.withOpacity(0.03),
   textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: color)),
-  colorScheme: ColorScheme.light(primary: color, secondary: const Color(0xFF329F93)).copyWith(
+  colorScheme: ColorScheme.light(primary: color, secondary: const Color(0xFF1A1A1A)).copyWith(
       surface: const Color(0xFFFCFCFC)).copyWith(error: const Color(0xFFE84D4F)),
   popupMenuTheme: const PopupMenuThemeData(color: Colors.white, surfaceTintColor: Colors.white),
   dialogTheme: const DialogTheme(surfaceTintColor: Colors.white),
